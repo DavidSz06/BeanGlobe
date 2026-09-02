8d428c597ef33af949811546d7b05a8b5bc5d015
Dávid Szilágyi <david.skb2006@gmail.com>
2026-09-02 23:09:28 +0300

    forgot to save xd

--- beanglobe/db/schema.sql
+++ beanglobe/db/schema.sql
@@ -185,3 +185,45 @@ ALTER TABLE public.retailers
 
 ALTER TABLE public.prices
     ENABLE ROW LEVEL SECURITY;
+
+--------------------------------------AUTH & USERS---------------------------------------------------------------------------------------------------
+
+CREATE TABLE public.profiles (
+    id UUID NOT NULL
+        REFERENCES auth.users
+        ON DELETE CASCADE,
+
+    role TEXT NOT NULL DEFAULT 'user'
+        CHECK (role IN ('user', 'admin', 'shop_owner')),
+
+    display_name TEXT,
+
+    created_at TIMESTAMPTZ NOT NULL
+        DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (id)
+);
+
+
+ALTER TABLE public.profiles
+    ENABLE ROW LEVEL SECURITY;
+
+
+CREATE FUNCTION public.handle_new_user()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+SECURITY DEFINER SET search_path = ''
+AS $$
+BEGIN
+    INSERT INTO public.profiles (id)
+    VALUES (NEW.id);
+
+    RETURN NEW;
+END;
+$$;
+
+
+CREATE TRIGGER on_auth_user_created
+    AFTER INSERT ON auth.users
+    FOR EACH ROW
+    EXECUTE PROCEDURE public.handle_new_user();
